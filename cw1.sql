b26e049a789d959f0b07d3c1497e8304787c6b15
greatdevan13-byte <greatdevan13@gmail.com>
2025-12-05 11:32:43 +0530

    Add initial SQL queries for student data retrieval

---
+++ cw1.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM `students`
+SELECT * FROM students WHERE Age>20;
+SELECT * FROM students WHERE Department= "Computer science" OR "Physics";
+SELECT * FROM students WHERE Department IN ("Computer science","Physics");
+SELECT * FROM `students`
+SELECT * FROM students WHERE Grade=90;
+SELECT * FROM students WHERE Grade BETWEEN 70 AND 90;
